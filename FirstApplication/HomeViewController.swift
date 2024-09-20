0fea4d4fd255a70373b38b81a84bc3b5528035de
aishwarya <ashp17star@gmail.com>
2024-09-20 12:26:18 +0530

    Creating Screen by using objects from Objects Library

--- FirstApplication/HomeViewController.swift
+++ FirstApplication/HomeViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var welcomeMessageLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        btnLogin.setTitle("Login", for: .normal)
+        self.imageView1.image = UIImage(named: "bitcode_image")
     }
     
+    
+    @IBAction func btnLoginClick(_ sender: Any) {
+        self.welcomeMessageLabel.text = "Welcome To " + usernameTextField.text!
+    }
 }
--- FirstApplication/ViewController.swift
+++ FirstApplication/ViewController.swift
@@ -8,69 +8,85 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    //creating UI through code
+    var welcomeLabel : UILabel?
+    var usernameTextField : UITextField?
+    var passwordTextField : UITextField?
+    var btnLogin : UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loginPageUI()
     }
     
-    
     func loginPageUI(){
-        
         let cgRectForLabel = CGRect(x: 100,
                                     y: 100,
                                     width: 200,
                                     height: 40)
         
-        let welcomeLabel = UILabel(frame: cgRectForLabel)
-        welcomeLabel.backgroundColor = .cyan
-        welcomeLabel.textColor = .red
-        welcomeLabel.textAlignment = .center
-        welcomeLabel.font = .systemFont(ofSize: 20.0, weight: .bold)
-        welcomeLabel.text = "Welcome To Bitcode"
+        welcomeLabel = UILabel(frame: cgRectForLabel)
+        welcomeLabel!.backgroundColor = .cyan
+        welcomeLabel!.textColor = .red
+        welcomeLabel!.textAlignment = .center
+        welcomeLabel!.font = .systemFont(ofSize: 20.0, weight: .bold)
+        welcomeLabel!.text = "Welcome To Bitcode"
         
-        
-        self.view.addSubview(welcomeLabel)
-        
+        self.view.addSubview(welcomeLabel!)
         
         let cgRectForUsernameTextField = CGRect(x: 100,
                                                 y: 160,
                                                 width: 200,
                                                 height: 40)
         
-        let usernameTextField = UITextField(frame: cgRectForUsernameTextField)
-        usernameTextField.placeholder = "Enter username"
-        usernameTextField.font = .systemFont(ofSize: 20.0, weight: .semibold)
-        usernameTextField.textColor = .brown
-        usernameTextField.textAlignment = .center
-        usernameTextField.borderStyle = .roundedRect
+        usernameTextField = UITextField(frame: cgRectForUsernameTextField)
+        usernameTextField!.placeholder = "Enter username"
+        usernameTextField!.font = .systemFont(ofSize: 20.0, weight: .semibold)
+        usernameTextField!.textColor = .brown
+        usernameTextField!.textAlignment = .center
+        usernameTextField!.borderStyle = .roundedRect
         
-        self.view.addSubview(usernameTextField)
+        self.view.addSubview(usernameTextField!)
         
         let cgRectForPasswordTextField = CGRect(x: 100,
                                                 y: 220,
                                                 width: 200,
                                                 height: 40)
         
-        let passwordTextField = UITextField(frame: cgRectForPasswordTextField)
-        passwordTextField.placeholder = "Enter Password"
-        passwordTextField.font = .systemFont(ofSize: 20.0, weight: .bold)
-        passwordTextField.textColor = .pink
-        passwordTextField.borderStyle = .line
-        passwordTextField.textAlignment = .justified
+        passwordTextField = UITextField(frame: cgRectForPasswordTextField)
+        passwordTextField!.placeholder = "Enter Password"
+        passwordTextField!.font = .systemFont(ofSize: 20.0, weight: .bold)
+        passwordTextField!.textColor = .pink
+        passwordTextField!.borderStyle = .line
+        passwordTextField!.textAlignment = .justified
         
-        self.view.addSubview(passwordTextField)
+        self.view.addSubview(passwordTextField!)
         
         let cgRectButton = CGRect(x: 100,
                                   y: 280,
                                   width: 200,
                                   height: 40)
+    
+        btnLogin = UIButton(frame: cgRectButton)
+        btnLogin!.setTitle("Login", for: .normal)
+        btnLogin!.setTitleColor(.black, for: .normal)
+        btnLogin!.backgroundColor = UIColor(named: "PinkColor")
         
-        let btnLogin = UIButton(frame: cgRectButton)
-        btnLogin.setTitle("Login", for: .normal)
-        btnLogin.setTitleColor(.black, for: .normal)
-        btnLogin.backgroundColor = UIColor(named: "PinkColor")
+        //Way 1 --> of adding action on button click by objective-C syntax
+        btnLogin!.addTarget(self,
+                           action: #selector(btnLoginClicked),
+                           for: .touchUpInside)
         
-        self.view.addSubview(btnLogin)
+        self.view.addSubview(btnLogin!)
+    }
+    
+    @objc func btnLoginClicked(){
+        self.welcomeLabel!.text = "Welcome To " + (usernameTextField?.text)!
     }
 }
